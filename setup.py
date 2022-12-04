07e54be416770a56724906b14665852e55a2be71
BlockArchitech <hello@blockarchitech.com>
2022-12-04 17:06:05 -0500

    Update setup.py, change license info

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name = 'pyledshop',
     packages = ['pyledshop'],
-    version = '0.0.2',
+    version = '0.0.1',
     description = 'A Python library to communicate with LED Shop compatible led strip controllers',
-    author = 'Kyle Zimmerman',
-    url = 'https://github.com/kylezimmerman/pyledshop',
+    author = 'blockarchitech',
+    url = 'https://github.com/blockarchitech/pyledshop',
     license = 'MIT',
     keywords = [
         'led shop',
